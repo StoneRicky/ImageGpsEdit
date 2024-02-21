42111122dc596ca0f3233fb6a7e0adff7d664fdc
StoneRicky <739799980@qq.com>
2024-02-21 17:12:49 +0800

    修改GPS数据同时，不覆盖其他数据

--- ImageGpsEdit.py
+++ ImageGpsEdit.py
@@ -2,12 +2,13 @@ from PIL import Image
 from PIL.ExifTags import TAGS
 import piexif
 from fractions import Fraction
+import pickle
 
 # 获取图片的GPS信息
 def get_gps_info(image_path):
     image = Image.open(image_path)
     exif_data = image._getexif()
-
+    print(exif_data)
     if exif_data is not None:
         for tag, value in exif_data.items():
             tag_name = TAGS.get(tag, tag)
@@ -21,25 +22,16 @@ def get_gps_info(image_path):
 def write_gps_info(image_path, gps_longitude,gps_latitude, gps_altitude):
     # 打开图片
     image = Image.open(image_path)
-
-    # 创建一个空的Exif数据
-    exif_dict = {"GPS": {}}
-
-    # 构建GPS信息
-    gps_info = {
-        piexif.GPSIFD.GPSLatitude: _convert_to_dms(gps_longitude),
-        piexif.GPSIFD.GPSLongitude: _convert_to_dms(gps_latitude),
-        piexif.GPSIFD.GPSAltitude: (gps_altitude,1000),  # 将高度转换为以毫米为单位
-    }
-
-    print(gps_info)
-
-    # 将GPS信息插入到Exif数据中
-    exif_dict["GPS"] = gps_info
+    # 修改GPS信息
+    exif_dict = piexif.load(image.info['exif'])
+    exif_dict['GPS'][piexif.GPSIFD.GPSLatitude] = _convert_to_dms(gps_longitude)
+    exif_dict['GPS'][piexif.GPSIFD.GPSLongitude] = _convert_to_dms(gps_latitude)
+    
 
     # # 将修改后的Exif数据重新写入图片
+    # print(exif_dict)
     exif_bytes = piexif.dump(exif_dict)
-    image.save(image_path, exif=exif_bytes)
+    image.save(image_path, "jpeg", exif=exif_bytes)
 
 def _convert_to_dms(coordinate):
     degrees = int(coordinate[0])
@@ -52,10 +44,10 @@ if __name__ == '__main__':
     image_path = 'D:\\old.JPG'
 
     gps_info = get_gps_info(image_path)
-    #                      经度          纬度        高度
-    print("图片的GPS信息：", gps_info[2],gps_info[4],gps_info[6])
-    gps_info[2] = (11.0, 10.0, 7.76)
-    gps_info[4] = (8.0, 28.0, 32.31)
+    #                      纬度          经度        高度
+    print("原GPS信息：", gps_info[2],gps_info[4],gps_info[6])
+    gps_info[2] = (1.0, 10.0, 7.76)
+    gps_info[4] = (4.0, 28.0, 32.31)
     gps_altitude = int(gps_info[6]*1000)
 
     write_gps_info(image_path,gps_info[2],gps_info[4],gps_altitude)
