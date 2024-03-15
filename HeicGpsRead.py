5f2e646a8ef3777a88ba15453ba04e30fd4297d0
StoneRicky <739799980@qq.com>
2024-03-15 10:31:51 +0800

    增加读取脚本 HEIC仅能读取，暂未研究修改方法

---
+++ HeicGpsRead.py
@@ -0,0 +1,33 @@
+from PIL import Image
+from PIL.ExifTags import TAGS
+from fractions import Fraction
+import exifread
+# 获取图片的GPS信息
+def get_gps_info(image_path):
+    exif_data = exifread.process_file(open(image_path,'rb'))
+    return exif_data
+
+
+
+# dms转十进制
+def dms_to_decimal(degree,minute,second):
+    decimal = degree + minute/60 + second/60/60
+    return decimal
+
+if __name__ == '__main__':
+    
+    image_path = 'D:\\IMG_3478.HEIC'
+
+    gps_info = get_gps_info(image_path)
+    
+    # 纬度       
+    Lat = gps_info.get('GPS GPSLatitude', '0').values
+    # 经度 
+    Long = gps_info.get('GPS GPSLongitude', '0').values
+    
+    # print(Lat)
+    print("GPS信息：\n",  Lat,"\n", Long,"\n")
+    print("十进制:")
+    # print(dms_to_decimal(Lat[0],Lat[1],Lat[2]))
+    print(dms_to_decimal(float(Lat[0]),float(Lat[1]),float(Lat[2])))
+    print(dms_to_decimal(float(Long[0]),float(Long[1]),float(Long[2])))
---
+++ ImageGpsRead.py
@@ -0,0 +1,35 @@
+from PIL import Image
+from PIL.ExifTags import TAGS
+from fractions import Fraction
+
+# 获取图片的GPS信息
+def get_gps_info(image_path):
+    image = Image.open(image_path)
+    exif_data = image._getexif()
+    # print(exif_data)
+    if exif_data is not None:
+        for tag, value in exif_data.items():
+            tag_name = TAGS.get(tag, tag)
+            if tag_name == 'GPSInfo':
+                print(value)
+                return value
+
+    return None
+
+
+# dms转十进制
+def dms_to_decimal(degree,minute,second):
+    decimal = degree + minute/60 + second/60/60
+    return decimal
+
+if __name__ == '__main__':
+    
+    image_path = 'D:\\IMG_3478.HEIC'
+
+    gps_info = get_gps_info(image_path)
+    
+        #                      纬度               经度             高度
+    print("GPS信息：\n", gps_info[2],"\n",gps_info[4],"\n",gps_info[6])
+    print("十进制:")
+    print(dms_to_decimal(float(gps_info[2][0]),float(gps_info[2][1]),float(gps_info[2][2])))
+    print(dms_to_decimal(float(gps_info[4][0]),float(gps_info[4][1]),float(gps_info[4][2])))
